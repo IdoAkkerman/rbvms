9d32e1cf0250fc4748b8096cc1580734942fc445
Ido Akkerman <I.Akkerman@tudelft.nl>
2025-02-25 16:11:38 +0100

    Add density + IC

--- cases/ventofoil-2D/foil.c
+++ cases/ventofoil-2D/foil.c
@@ -1,68 +1,55 @@
 //--------------------------------------------------------------
-// Solution function for the von Karman vortex street
+// Function for foil simulation
 //
 // To compile run, for example:
-// mpicc -shared -o libvkvs.so -fPIC vkvs.c
-// gcc   -shared -o libvkvs.so -fPIC vkvs.c
+// mpicc -shared -o foil.so -fPIC foil.c
+// gcc   -shared -o foil.so -fPIC foil.c
 //--------------------------------------------------------------
 
 #include <math.h>
 #include <stdio.h>
 
+// Solution: IC & BC
+// t = -1.0 at IC
 void sol_u(double *coord, int dim, double time, double *u, int vdim)
 {
    double x = coord[0]-1.15;
    double y = coord[1]-0.65;
    double r = sqrt(x*x + y*y);
-   double r0 = 2.0;
+   double uinf = 10.0;
 
    // Base flow + BC
-   u[0] =  r < 1.5 ? 0.0: 10.0 ;
+   u[0] = (r < 1.5 && time > 0.0) ?  0.0 : uinf;
    u[1] = 0.0;
-/*
-   // Disturbance 1
-   x = coord[0];
-   y = coord[1] - 3.0;
-   r = sqrt(x*x + y*y);
-   r0 = 1.0;
-   u[0] += 2.0*fmax(0.0, r0 - r);
-   u[1] -= 0.5*fmax(0.0, r0 - r);
-
-   // Disturbance 2
-   x = coord[0] + 3.0;
-   y = coord[1];
-   r = sqrt(x*x + y*y);
-   r0 = 1.0;
-   u[0] -= 0.5*fmax(0.0, r0 - r);
-   u[1] += 2.0*fmax(0.0, r0 - r);*/
 }
 
+// Force: body force in N/m^3
 void force(double *coord, int dim, double time, double *force, int vdim)
 {
    force[0] = 0.0;
    force[1] = 0.0;
 }
 
-void traction(double *coord, int dim, double time, double *traction, int vdim)
-{
-   traction[0] = 0.0;
-   traction[1] = 0.0;
-}
-
-// Suction pressure
+// Suction: pressure in Pa
 double suction(double *coord, int dim, double time)
 {
-   return -225.0;
+   return -250.0;
 }
 
-// Blowing velocity
+// Blowing: normal velocity in m/s
 double blowing(double *coord, int dim, double time)
 {
-   return 1.978*fmin(1.0, 1e-3*exp(4.0*time/1.0));
+   return 1.978*fmin(1.0, 10*exp(10.0*time/1.0));
 }
 
+// Density of air in kg/m^3 at 273 K (20C) & 1.01325 bar (1 atm)
+double rho(double *coord, int dim, double time)
+{
+   return 1.204;
+}
+
+// Dynamic viscosity of air kg/ms at 273 K (20C) & 1.01325 bar (1 atm)
 double mu(double *coord, int dim, double time)
 {
-   return 1.48e-5*fmax(1.0, 1e4*exp(-4.0*time/1.0));
+   return 1.825e-5*fmax(1.0, 1e4*exp(-100.0*time/1.0));
 }
-
