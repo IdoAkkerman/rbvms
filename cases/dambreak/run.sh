4b2177db6f92105e3778656d16ebe03c332d4be6
Ido Akkerman <I.Akkerman@tudelft.nl>
2025-03-05 17:30:52 +0100

    Small changes to 2D dambrak problem

--- cases/dambreak/run.sh
+++ cases/dambreak/run.sh
@@ -1,7 +1,8 @@
 mpicc -shared -o rbvms-ls.so -fPIC dambreak.c
 #gmsh -2 dambreak.geo -format msh22 -clscale 0.025
 
+rm solution/*
 
-mpirun -n 1 /home/ido/data/rbvms-ls2/deb/rbvms-ls/rbvms-ls -l rbvms-ls.so -m dambreak.msh --strong-bdr "1" -s 45 --dt_vis 0.01 \
---newton-tolerance 1e-8 --newton-itermax 30
+mpirun -n 16 /home/ido/data/rbvms-ls2/deb/rbvms-ls/rbvms-ls -l rbvms-ls.so -m dambreak.msh --weak-bdr "1" -s 45 -dt 0.0005 --dt_vis 0.01 \
+--newton-tolerance 1e-4 --newton-itermax 20 | tee log
  
