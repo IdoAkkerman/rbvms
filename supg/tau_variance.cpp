25d1ba3232b4c656b3c12dc834aa572166adb4ab
McSeem <applejack188288@gmail.com>
2025-12-08 00:40:27 +0100

    Update tau variance test to reflect changes in integrator

--- supg/tau_variance.cpp
+++ supg/tau_variance.cpp
@@ -39,18 +39,19 @@ static std::string BuildSingleTriMeshText(const std::array<std::array<double,2>,
    return os.str();
 }
 
-static double TauFromG(const DenseMatrix &G, const Vector &a, double k)
+static double TauFromG(const DenseMatrix &G, const Vector &a, double k, double Ch)
 {
-   // Tau formula copied from supg/integrator.cpp
-   // Standalone, so we don't have to instantiate
-   const double CI = 1.0/12.0;
+   // Mirror supg/integrator.cpp: StabConvDifIntegrator::GetTau
+   // Production code does: Ch = 1/Ch; and then adds (Ch*Ch*k*k) inside the i,j loop
+   // i.e. acc = 1e-10 + sum_{i,j} ( G(i,j) a[i] a[j] + (1/Ch)^2 k^2 )
+   double invCh = 1.0 / Ch;
    double tau_acc = 1e-10;
    const int dim = G.Width();
    for (int j = 0; j < dim; j++)
    {
       for (int i = 0; i < dim; i++)
       {
-         tau_acc += G(i,j) * a[i] * a[j] + CI*CI * k * k * G(i,j) * G(i,j);
+         tau_acc += G(i,j) * a[i] * a[j] + invCh * invCh * k * k;
       }
    }
    return 1.0 / std::sqrt(tau_acc);
@@ -58,7 +59,7 @@ static double TauFromG(const DenseMatrix &G, const Vector &a, double k)
 
 static void ComputeTausForMeshText(const std::string &mesh_text,
                                    double &tau_old, double &tau_new,
-                                   const Vector &a, double k)
+                                   const Vector &a, double k, double Ch)
 {
    std::istringstream is(mesh_text);
    Mesh mesh(is, 1, 0, false); // generate_nodes=1 to get Nodes if needed, do not fix orientation
@@ -77,7 +78,7 @@ static void ComputeTausForMeshText(const std::string &mesh_text,
    // Old method: G = invJ^T invJ
    DenseMatrix G_old(invJ.Width());
    MultAtB(invJ, invJ, G_old);
-   tau_old = TauFromG(G_old, a, k);
+   tau_old = TauFromG(G_old, a, k, Ch);
 
    // New method using GeomToPerfGeomJac
    const DenseMatrix &A = Geometries.GetGeomToPerfGeomJac(T->GetGeometryType());
@@ -85,15 +86,16 @@ static void ComputeTausForMeshText(const std::string &mesh_text,
    Mult(A, invJ, invJ_perf);
    DenseMatrix G_new(invJ.Width());
    MultAtB(invJ_perf, invJ_perf, G_new);
-   tau_new = TauFromG(G_new, a, k);
+   tau_new = TauFromG(G_new, a, k, Ch);
 }
 
 int main(int argc, char** argv)
 {
    // Advection and diffusion parameters to play around
-   double ax = 1.0, ay = 0.5, k = 0.0;
+   double ax = 1.0, ay = 0.5, k = 0.0, Ch = 12.0;
    if (argc >= 3) { ax = atof(argv[1]); ay = atof(argv[2]); }
    if (argc >= 4) { k = atof(argv[3]); }
+   if (argc >= 5) { Ch = atof(argv[4]); }
 
    Vector a(2); a[0] = ax; a[1] = ay;
 
@@ -107,7 +109,7 @@ int main(int argc, char** argv)
       std::array<std::array<double,2>,3> v = { base[perm[0]], base[perm[1]], base[perm[2]] };
       std::string mesh_text = BuildSingleTriMeshText(v);
       double to=0.0, tn=0.0;
-      ComputeTausForMeshText(mesh_text, to, tn, a, k);
+      ComputeTausForMeshText(mesh_text, to, tn, a, k, Ch);
       taus_old.push_back(to);
       taus_new.push_back(tn);
       perms_record.push_back(perm);
@@ -126,7 +128,7 @@ int main(int argc, char** argv)
 
    cout << "Tau variance test over all 6 permutations of a single triangle" << endl;
    cout << "Advective velocity a = (" << a[0] << ", " << a[1] << ")";
-   cout << ", k = " << k << endl;
+   cout << ", k = " << k << ", Ch = " << Ch << endl;
 
    cout.setf(std::ios::scientific); cout.precision(6);
    cout << "Old method (raw invJ):    mean=" << m_old << ", var=" << v_old
