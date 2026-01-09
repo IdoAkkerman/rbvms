992a67c7fad1eaa4399f50dd98421ec4687a6e20
CarlVistisen <110404922+CarlVistisen@users.noreply.github.com>
2026-01-09 10:31:59 +0100

    Add files via upload

---
+++ util/CSV_reader_L2.py
@@ -0,0 +1,18 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=pd.read_csv("L2file.csv")
+
+h=np.array(data.h)
+L2=np.array(data.L2)
+print(L2)    
+    
+plt.title("L2-error versus h")
+plt.plot(h,L2)
+plt.xlabel("h")
+plt.ylabel("L2-error")
+plt.show()
+
+
