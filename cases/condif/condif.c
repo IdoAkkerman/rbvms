a59af22ea9a438387b1f1d818ca64837c7be99b2
Anthony Chen <a.z.chen@student.tudelft.nl>
2026-01-14 11:02:34 +0100

    add macros to define advec angle

--- cases/condif/condif.c
+++ cases/condif/condif.c
@@ -9,6 +9,9 @@
 #include <math.h>
 #include <stdio.h>
 
+#ifndef ADVEC_ANGLE
+#define ADVEC_ANGLE 45
+#endif
 
 double sol_phi(double *coord, int dim, double time)
 {
@@ -17,8 +20,8 @@ double sol_phi(double *coord, int dim, double time)
 
 void advection(double *coord, int dim, double time, double *adv, int vdim)
 {
-   adv[0] = sqrt(2.0)/2.0;
-   adv[1] = sqrt(2.0)/2.0;
+   adv[0] = cos(ADVEC_ANGLE*(M_PI/180));
+   adv[1] = sin(ADVEC_ANGLE*(M_PI/180));
 }
 
 double force(double *coord, int dim, double time)
--- cases/condif_skew_adv/condif.c
+++ cases/condif_skew_adv/condif.c
@@ -9,6 +9,9 @@
 #include <math.h>
 #include <stdio.h>
 
+#ifndef ADVEC_ANGLE
+#define ADVEC_ANGLE 30
+#endif
 
 double sol_phi(double *coord, int dim, double time)
 {
@@ -19,8 +22,8 @@ double sol_phi(double *coord, int dim, double time)
 
 void advection(double *coord, int dim, double time, double *adv, int vdim)
 {
-   adv[0] = sqrt(3.0)/2.0;
-   adv[1] = 1.0/2.0;
+   adv[0] = cos(ADVEC_ANGLE*(M_PI/180));
+   adv[1] = sin(ADVEC_ANGLE*(M_PI/180));
 }
 
 double force(double *coord, int dim, double time)
