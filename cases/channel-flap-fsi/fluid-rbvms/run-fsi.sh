9b21353a8777e568b6850c4c89fff13c58e8a1de
Ido Akkerman <I.Akkerman@tudelft.nl>
2026-05-28 10:07:09 +0200

    Correct input file for fluid2

--- cases/channel-flap-fsi/fluid-rbvms/run-fsi.sh
+++ cases/channel-flap-fsi/fluid-rbvms/run-fsi.sh
@@ -11,7 +11,7 @@ rm output* log*
 
 ##valgrind --tool=memcheck \
 mpirun -n 1 $exe -m $mesh -o 1 -r 0 \
---dyn-visc 0.001 --density 1.0 \
+--dyn-visc 0.01 --density 1.0 \
 --strong-bdr "2 4 5" --outflow-bdr 3 --fsi-bdr 6 -l channel-flap-fsi.so \
 -s 21 -dt 0.01 --dt_vis 0.01 -tf 2.5 \
--lt 1e-3 -ni 50  -ri 10  -pfs 250 | tee log0
+-lt 1e-3 -ni 50  -ri 10  -pfs 100 | tee log0
